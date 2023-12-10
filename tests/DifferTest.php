4d2e9cc08e2d674f6089a5dd127f5c7a1c34cce3
julia <uliotapchenko@yandex.ru>
2023-12-10 14:30:47 +0300

    report

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -11,10 +11,10 @@ class DifferTest extends TestCase
     public function testGendiffJson(): void
     {
         $expected1 = file_get_contents("./tests/fixtures/SuccessGenDiffJson.txt");
-        //$this->assertEquals($expected1, gendiff("./tests/fixtures/file1.json", "./tests/fixtures/file2.json", "json"));
+        $this->assertEquals($expected1, gendiff("./tests/fixtures/file1.json", "./tests/fixtures/file2.json", "json"));
 
         $expected2 = file_get_contents("tests/fixtures/SuccessGenDiffStylish.txt");
-        //$this->assertEquals($expected2, gendiff("./tests/fixtures/file1.yaml", "./tests/fixtures/file2.yaml", "stylish"));
+        $this->assertEquals($expected2, gendiff("./tests/fixtures/file1.yaml", "./tests/fixtures/file2.yaml", "stylish"));
 
         $expected3 = file_get_contents("tests/fixtures/SuccessGenDiffPlain.txt");
         $this->assertEquals($expected3, gendiff("./tests/fixtures/file1.yaml", "./tests/fixtures/file2.yaml", "plain"));
